362b2d06077a974f46ab24d6d06cd0df5cfc9bf4
Frank Königstein <frank.koenigstein@circlon.de>
2018-10-10 14:52:49 +0200

    doc: add honeywell mobility sdk

---
+++ doc/Honeywell_MobilitySDK_Android_v1.00.00.0054/honeywell-android-data-collection-sdk/samples/BarcodeExample/app/src/main/java/com/honeywell/barcodeexample/AutomaticBarcodeActivity.java
@@ -0,0 +1,144 @@
+package com.honeywell.barcodeexample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.honeywell.aidc.*;
+
+public class AutomaticBarcodeActivity extends Activity implements BarcodeReader.BarcodeListener,
+        BarcodeReader.TriggerListener {
+
+    private com.honeywell.aidc.BarcodeReader barcodeReader;
+    private ListView barcodeList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+
+        // set lock the orientation
+        // otherwise, the onDestory will trigger when orientation changes
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // get bar code instance from MainActivity
+        barcodeReader = MainActivity.getBarcodeObject();
+
+        if (barcodeReader != null) {
+
+            // register bar code event listener
+            barcodeReader.addBarcodeListener(this);
+
+            // set the trigger mode to client control
+            try {
+                barcodeReader.setProperty(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE,
+                        BarcodeReader.TRIGGER_CONTROL_MODE_AUTO_CONTROL);
+            } catch (UnsupportedPropertyException e) {
+                Toast.makeText(this, "Failed to apply properties", Toast.LENGTH_SHORT).show();
+            }
+            // register trigger state change listener
+            barcodeReader.addTriggerListener(this);
+
+            Map<String, Object> properties = new HashMap<String, Object>();
+            // Set Symbologies On/Off
+            properties.put(BarcodeReader.PROPERTY_CODE_128_ENABLED, true);
+            properties.put(BarcodeReader.PROPERTY_GS1_128_ENABLED, true);
+            properties.put(BarcodeReader.PROPERTY_QR_CODE_ENABLED, true);
+            properties.put(BarcodeReader.PROPERTY_CODE_39_ENABLED, true);
+            properties.put(BarcodeReader.PROPERTY_DATAMATRIX_ENABLED, true);
+            properties.put(BarcodeReader.PROPERTY_UPC_A_ENABLE, true);
+            properties.put(BarcodeReader.PROPERTY_EAN_13_ENABLED, false);
+            properties.put(BarcodeReader.PROPERTY_AZTEC_ENABLED, false);
+            properties.put(BarcodeReader.PROPERTY_CODABAR_ENABLED, false);
+            properties.put(BarcodeReader.PROPERTY_INTERLEAVED_25_ENABLED, false);
+            properties.put(BarcodeReader.PROPERTY_PDF_417_ENABLED, false);
+            // Set Max Code 39 barcode length
+            properties.put(BarcodeReader.PROPERTY_CODE_39_MAXIMUM_LENGTH, 10);
+            // Turn on center decoding
+            properties.put(BarcodeReader.PROPERTY_CENTER_DECODE, true);
+            // Enable bad read response
+            properties.put(BarcodeReader.PROPERTY_NOTIFICATION_BAD_READ_ENABLED, true);
+            // Apply the settings
+            barcodeReader.setProperties(properties);
+        }
+
+        // get initial list
+        barcodeList = (ListView) findViewById(R.id.listViewBarcodeData);
+    }
+
+    @Override
+    public void onBarcodeEvent(final BarcodeReadEvent event) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // update UI to reflect the data
+                List<String> list = new ArrayList<String>();
+                list.add("Barcode data: " + event.getBarcodeData());
+                list.add("Character Set: " + event.getCharset());
+                list.add("Code ID: " + event.getCodeId());
+                list.add("AIM ID: " + event.getAimId());
+                list.add("Timestamp: " + event.getTimestamp());
+
+                final ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(
+                        AutomaticBarcodeActivity.this, android.R.layout.simple_list_item_1, list);
+
+                barcodeList.setAdapter(dataAdapter);
+            }
+        });
+    }
+
+    // When using Automatic Trigger control do not need to implement the
+    // onTriggerEvent function
+    @Override
+    public void onTriggerEvent(TriggerStateChangeEvent event) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onFailureEvent(BarcodeFailureEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (barcodeReader != null) {
+            try {
+                barcodeReader.claim();
+            } catch (ScannerUnavailableException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Scanner unavailable", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (barcodeReader != null) {
+            // release the scanner claim so we don't get any scanner
+            // notifications while paused.
+            barcodeReader.release();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (barcodeReader != null) {
+            // unregister barcode event listener
+            barcodeReader.removeBarcodeListener(this);
+
+            // unregister trigger state change listener
+            barcodeReader.removeTriggerListener(this);
+        }
+    }
+}
---
+++ doc/Honeywell_MobilitySDK_Android_v1.00.00.0054/honeywell-android-data-collection-sdk/samples/BarcodeExample/app/src/main/java/com/honeywell/barcodeexample/MainActivity.java
@@ -0,0 +1,112 @@
+package com.honeywell.barcodeexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.honeywell.aidc.AidcManager;
+import com.honeywell.aidc.AidcManager.CreatedCallback;
+import com.honeywell.aidc.BarcodeReader;
+
+public class MainActivity extends Activity {
+
+    private static BarcodeReader barcodeReader;
+    private AidcManager manager;
+
+    private Button btnAutomaticBarcode;
+    private Button btnClientBarcode;
+    private Button btnScannerSelectBarcode;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // set lock the orientation
+        // otherwise, the onDestory will trigger
+        // when orientation changes
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // create the AidcManager providing a Context and a
+        // CreatedCallback implementation.
+        AidcManager.create(this, new CreatedCallback() {
+
+            @Override
+            public void onCreated(AidcManager aidcManager) {
+                manager = aidcManager;
+                barcodeReader = manager.createBarcodeReader();
+            }
+        });
+
+        ActivitySetting();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    static BarcodeReader getBarcodeObject() {
+        return barcodeReader;
+    }
+
+    /**
+     * Create buttons to launch demo activities.
+     */
+    public void ActivitySetting() {
+        btnAutomaticBarcode = (Button) findViewById(R.id.buttonAutomaticBarcode);
+        btnAutomaticBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the intent action string from AndroidManifest.xml
+                Intent barcodeIntent = new Intent("android.intent.action.AUTOMATICBARCODEACTIVITY");
+                startActivity(barcodeIntent);
+            }
+        });
+
+        btnClientBarcode = (Button) findViewById(R.id.buttonClientBarcode);
+        btnClientBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the intent action string from AndroidManifest.xml
+                Intent barcodeIntent = new Intent("android.intent.action.CLIENTBARCODEACTIVITY");
+                startActivity(barcodeIntent);
+            }
+        });
+
+        btnScannerSelectBarcode = (Button) findViewById(R.id.buttonScannerSelectBarcode);
+        btnScannerSelectBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the intent action string from AndroidManifest.xml
+                Intent barcodeIntent = new Intent(
+                        "android.intent.action.SCANNERSELECTBARCODEACTIVITY");
+                startActivity(barcodeIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (barcodeReader != null) {
+            // close BarcodeReader to clean up resources.
+            barcodeReader.close();
+            barcodeReader = null;
+        }
+
+        if (manager != null) {
+            // close AidcManager to disconnect from the scanner service.
+            // once closed, the object can no longer be used.
+            manager.close();
+        }
+    }
+
+}
