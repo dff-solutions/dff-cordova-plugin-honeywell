6a821d5c2ac4f318550df0c75a07d3f72dc3b51f
Matthias Dittmer <mail@matthiasdittmer.de>
2018-10-15 12:21:47 +0200

    add: HoneywellAction now abstract. Add aidcManager and barcodeListener.

--- src/main/java/com/dff/cordova/plugin/honeywell/barcode/action/HoneywellAction.java
+++ src/main/java/com/dff/cordova/plugin/honeywell/barcode/action/HoneywellAction.java
@@ -1,5 +1,7 @@
 package com.dff.cordova.plugin.honeywell.barcode.action;
 
+import com.dff.cordova.plugin.honeywell.barcode.BarcodeListener;
+import com.honeywell.aidc.AidcManager;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
@@ -7,13 +9,17 @@ import org.json.JSONArray;
 import com.dff.cordova.plugin.common.action.CordovaAction;
 import com.honeywell.aidc.BarcodeReader;
 
-public class HoneywellAction extends CordovaAction {
+public abstract class HoneywellAction extends CordovaAction {
 	protected BarcodeReader barcodeReader;
+	protected AidcManager aidcManager;
+	protected BarcodeListener barcodeListener;
 
 	public HoneywellAction(String action, JSONArray args, CallbackContext callbackContext, CordovaInterface cordova,
-	        BarcodeReader barcodeReader) {
+	        BarcodeReader barcodeReader,  AidcManager aidcManager, BarcodeListener barcodelistener) {
 		super(action, args, callbackContext, cordova);
 
 		this.barcodeReader = barcodeReader;
+		this.aidcManager = aidcManager;
+		this.barcodeListener = barcodelistener;
 	}
 }
